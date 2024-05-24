3477082ca6eae818630f1c49df415fb2581460f2
Tiago <tiagomanosso@gmail.com>
2024-05-24 14:01:14 -0300

    split

--- components/url-util.js
+++ components/url-util.js
@@ -4,8 +4,10 @@ export const urlUtil = {
     extract: (params) => {
         try {
             if (params.includes('minisite')) {
-                const username = params.split('&')[0].split('?')[1];
-                const store = params.split('=')[1];
+                // const username = params.split('&')[0].split('?')[1];
+                // const store = params.split('=')[1];
+                const username = params.split('&')[1].replace('=', '')
+                const store = params.split('=')[1].split('&')[0];
                 params = `/${username}/${store}`
             } else if (params.includes('?')) {
                 const username = params.split('&')[0]
