39b79bbe0d8a83411fc4698400a103d8f16867a1
Tiago <tiagomanosso@gmail.com>
2024-05-24 14:48:51 -0300

    crenndioosss

--- components/url-util.js
+++ components/url-util.js
@@ -3,26 +3,36 @@ import { URL } from "url";
 export const urlUtil = {
     extract: (params) => {
         try {
+            params = '/minisite?store=642&caparceirando='
             if (params.includes('minisite')) {
                 const i = params.split('&')
                 console.log("i : ", i)
                 const u = i[0].split('?')
                 console.log("u : ", u)
-                const store = i[1].split('=')[1];
-                if (u[1].includes('=')) {
-                    const username = u[1].replaceAll('=', '')
+                let store = i[1].split('=')[1];
+                let username = '';
+                if (i[0].includes('store')) {
+                    const is = i[0].split('?')
+                    store = is[1].split('=')[1];
+                    console.log("SPLIT store: ", store)
+                    i[1].includes('=') ? username = i[1].split('=')[0] : username = i[1]
+                    console.log("SPLIT username: ", username)
                     params = `/${username}/${store}`
                 } else {
-                    // const username = params.split('&')[0].split('?')[1];
-                    // const store = params.split('=')[1];
-                    const username = params.split('&')[1]
-                    if (username.includes('=')) {
-                        console.log("SPLIT: ", username)
+                    if (u[1].includes('=')) {
+                        username = u[1].replaceAll('=', '')
+                        params = `/${username}/${store}`
+                    } else {
+                        // const username = params.split('&')[0].split('?')[1];
+                        // const store = params.split('=')[1];
+                        username = params.split('&')[1]
+                        if (username.includes('=')) {
+                            console.log("SPLIT: ", username)
+                        }
+                        store = params.split('=')[1].split('&')[0];
+                        params = `/${username}/${store}`
                     }
-                    const store = params.split('=')[1].split('&')[0];
-                    params = `/${username}/${store}`
                 }
-
             } else if (params.includes('?')) {
                 const username = params.split('&')[0]
                 const store = params.split('=')[1];
