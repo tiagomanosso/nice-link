8f1ac26e1b6eea837c5d5c7f67fbe9381833c208
Tiago <tiagomanosso@gmail.com>
2024-05-24 12:49:26 -0300

    d

--- components/url-util.js
+++ components/url-util.js
@@ -3,11 +3,12 @@ import { URL } from "url";
 export const urlUtil = {
     extract: (params) => {
         try {
+            console.log("params", params)
             if (params.includes('minisite')) {
                 const username = params.split('&')[0].split('?')[1];
                 const store = params.split('=')[1];
                 params = `/${username}/${store}`
-                console.log(params)
+                
             } else if (params.includes('?')) {
                 const username = params.split('&')[0]
                 const store = params.split('=')[1];
