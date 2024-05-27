2df30806b85739d587e54f71aeddebe5aed1169f
Tiago <tiagomanosso@gmail.com>
2024-05-27 11:23:43 -0300

    title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import sDefault from '../next-seo.config';
 export default function Home({ bioData, seoData }) {
 
   const page = {
-    title: bioData?.name || 'Parceirando',
+    title: bioData?.title || 'Parceirando',
     excerpt: 'home',
     slug: '/',
     coverImage: bioData?.profileImageUrl || 'https://parceirando-minisite-images.s3.amazonaws.com/site/642/642.png',
